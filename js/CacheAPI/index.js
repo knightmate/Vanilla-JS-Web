b3eb2bcc03757887e8302a7063574ee3c8b88bd8
Meghraj Deshmukh <meghraj.deshmukh103@gmail.com>
2023-12-28 01:10:24 +0530

    logic improved ;

--- js/CacheAPI/index.js
+++ js/CacheAPI/index.js
@@ -13,33 +13,23 @@ const fakeAPICall=(URL)=>{
 }
 
 //implement cachedAPICall
-function cachedAPICall(cacheTime){
+function cachedAPICall(cacheTime,config){
    
     const cacheResponse=new Map();
-    const key="API_RESPONSE_CACHE";
-   //{data:some, time:new Date().toString()}
+    //{data:some, time:new Date().toString()}
 
-    return (url,obj)=>{
+    return async(url,obj)=>{
+        const key=JSON.stringify(obj);//"API_RESPONSE_CACHE";
+        const cacheData=cacheResponse.get(key)
+            
+        if(!cacheData || Date.now()>cacheData.time){
 
-       return new Promise((res,rej)=>{
-
-        const cacheData=cacheResponse.get(key)||null;
-        const isCacheValid= cacheData && cacheData.time?Date.now()-cacheData.time<cacheTime?true:false:false;
-           
-         if(isCacheValid){
-            //validate cache time
-        //if valid return the same else call again and cache
-          return res(cacheData.data)
+            const response=await fakeAPICall(url);
+            cacheResponse.set(key,{data:response,time:Date.now()+cacheTime})
+            return response;
         }
-        fakeAPICall(url).then((result)=>{
-            //save api response into map
-
-            cacheResponse.set(key,{data:result,time:Date.now()})
-             
-            res(result)
-        });
-
-       })
+        return cacheData.data;
+       
 
     }
 
@@ -50,7 +40,7 @@ const call=cachedAPICall(1500);
 
 const callAPI=()=>{
 
-     call('API url',{}).then((res)=>console.log("Response from API call"+res));
+     call('API url',{name:"Meghraj"}).then((res)=>console.log("Response from API call"+res));
 
 }
  
@@ -60,14 +50,16 @@ const callAPI=()=>{
 const CacheAPICall=()=>{
 
     callAPI();
+    call('API url',{name:"AMU"}).then((res)=>console.log("Response from API call"+res));
+
     setTimeout(()=>{
         console.log("Calling after 1s--")
         callAPI();
     },800);
-    setTimeout(()=>{
-        console.log("Calling after 2s--")
-        callAPI();
-    },3000);
+    // setTimeout(()=>{
+    //     console.log("Calling after 2s--")
+    //     callAPI();
+    // },3000);
 
 }
  export default CacheAPICall;
