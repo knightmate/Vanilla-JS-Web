89b79ba7dff7414bb3e2f9a58357b52b0309a88c
Meghraj Deshmukh <meghraj.deshmukh103@gmail.com>
2023-12-29 00:22:21 +0530

    sovled

--- Get_Obj_From_String/index.js
+++ Get_Obj_From_String/index.js
@@ -1,41 +1,45 @@
+ 
 
-
-
-
+function splitStringToArray(inputString) {
+    return inputString.replace(/\[/g, '.').replace(/\]/g, '').split('.');
+}
+ 
 
 const get=(obj,path)=>{
 
     //for arrray
     //first we find how to split
-    const isArray=path.includes('[')?true:false;
-
-    let targets=path.includes("[")?path.split(""):path.split('.');
-      if(targets.includes("[")){
-        targets=targets.filter((val)=>parseInt(val))
-     }
-    
+ 
+     
+    let targets=splitStringToArray(path)
+ 
+     
+     
      let temp=obj;
       
      targets.forEach(target=> {
        
-         temp=temp[target]
 
+        if(  target!==""){
+             
+            temp=temp[target]
+        }
+ 
      });
-     console.log("target",temp,targets);
-    
 
+     console.log(temp,targets)
+ 
 }
 
 
 export default   function GetObjectFromString(){
 
-    //get({developer:"Software Engineer"},"devloper") //"Software Engineer"
-
+     get({developer:"Software Engineer"},"developer") //"Software Engineer"
      get({developer:{firstName:"TOm",lastName:"Cruz"}},"developer.lastName") //"Cruz "
-     get ([{developer:"tom"},{count:[0,1]}],"[1].count[0]")//o
-    // get([{developer:"Tom"},[0,null],"[1][1]"])//null
-
- 
+     get([{developer:"tom"},{count:[0,1]}],"[1].count[0]")//o
+     get([{developer:"Tom"},[0,null]],"[1][1]")//null
+      
+  
 
 
 
